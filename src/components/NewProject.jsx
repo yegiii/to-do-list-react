a3a6c803f5d1a49a3a76c4f7e73830f30f3fc75f
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-01 11:58:01 +0330

    Add styles

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -11,26 +11,29 @@ export default function AddProject({ onSubmit }) {
   }
 
   return (
-    <>
-      <div>
-        <menu className="flex gap-2 ml-auto">
-          <li>
-            <button className="">Cancel</button>
-          </li>
-          <li>
-            <button type="submit" className="rounded">
-              Save
-            </button>
-          </li>
-        </menu>
-        <div className="flex flex-col items-center w-full pl-8 pr-16">
-          <Input label="Title" />
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancel
+          </button>
+        </li>
+        <li>
+          <button
+            type="submit"
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
+            Save
+          </button>
+        </li>
+      </menu>
+      <div className="">
+        <Input label="Title" />
 
-          <Input label="Description" textarea />
+        <Input label="Description" textarea />
 
-          <Input label="Due Date" />
-        </div>
+        <Input label="Due Date" />
       </div>
-    </>
+    </div>
   );
 }
