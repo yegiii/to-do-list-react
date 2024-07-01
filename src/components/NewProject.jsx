01622d5bcf108f824b36b282b0219b934bb67e1c
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-01 11:47:21 +0330

    Use Input.jsx in NewProject.jsx

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import Input from "./Input";
 
 export default function AddProject({ onSubmit }) {
   const [project, setProject] = useState({ title: "", desc: "", dueDate: "" });
@@ -14,50 +15,20 @@ export default function AddProject({ onSubmit }) {
       <div>
         <menu className="flex gap-2 ml-auto">
           <li>
-            <button className="text-black">Cancel</button>
+            <button className="">Cancel</button>
           </li>
           <li>
-            <button
-              type="submit"
-              className="rounded bg-black h-8 px-4 text-white "
-            >
+            <button type="submit" className="rounded">
               Save
             </button>
           </li>
         </menu>
-        <div
-          onSubmit={(event) => onSubmit(event, project)}
-          className="flex flex-col items-center w-full pl-8 pr-16"
-        >
-          <p>
-            <label className="uppercase w-full">title</label>
-            <input
-              className="block w-full text-gray-800"
-              onChange={(event) => handleInputs(event, "title")}
-              value={project.title}
-              type="text"
-            />
-          </p>
+        <div className="flex flex-col items-center w-full pl-8 pr-16">
+          <Input label="Title" />
 
-          <p>
-            <label className="uppercase w-full">description</label>
-            <textarea
-              onChange={(event) => handleInputs(event, "desc")}
-              className="block w-full text-gray-800"
-              value={project.desc}
-            />
-          </p>
+          <Input label="Description" textarea />
 
-          <p>
-            <label className="uppercase w-full">due date</label>{" "}
-            <input
-              onChange={(event) => handleInputs(event, "dueDate")}
-              value={project.dueDate}
-              className="block w-full text-gray-800"
-              type="date"
-              name="date"
-            />
-          </p>
+          <Input label="Due Date" />
         </div>
       </div>
     </>
