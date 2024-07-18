96979ed92cd29b29a45b66c2abfda5266b809408
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-18 06:30:02 +0330

    Add style and use Button component in Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,5 +1,6 @@
 import { createPortal } from "react-dom";
 import { forwardRef, useImperativeHandle, useRef } from "react";
+import Button from "./Button";
 
 const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   const dialog = useRef();
@@ -13,10 +14,13 @@ const Modal = forwardRef(function Modal({ children, buttonCaption }, ref) {
   });
 
   return createPortal(
-    <dialog ref={dialog}>
+    <dialog
+      ref={dialog}
+      className="backdrop:bg-stone-900/90 p-4 rounded-lg shadow-md"
+    >
       {children}
-      <form action="dialog">
-        <button>{buttonCaption}</button>
+      <form action="dialog" className="mt-4 text-right">
+        <Button>{buttonCaption}</Button>
       </form>
     </dialog>,
     document.getElementById("modal-root")
