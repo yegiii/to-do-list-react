8486cdadf2bd37e2d3382a2efea975a94e828cb4
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-18 06:28:28 +0330

    Tidy up

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -7,107 +7,58 @@ import AddTask from "./AddTask";
 
 export default function Content() {
   const [projectsState, setProjectsState] = useState({
-    //currentAction : 'adding'  =>'adding' , 'selected-project' , 'nothing-selected
+    //currentAction : 'adding'  =>'adding' , 'selected-project' , 'nothing-selected'
     // for controlling what's been displayed
     selectedProjectId: undefined,
     projects: [],
   });
 
+  // Means we dont have any project and we want to add one
   function handleStartAddProject() {
     setProjectsState((prevState) => {
       return { ...prevState, selectedProjectId: null };
     });
   }
 
+  // We want to add a new project
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const projectId = Math.random();
       const newProject = {
-        // let id = prevPros.length == 0 ? 0 : prevPros[0].id + 1;
-        // let newProject = { tasks: [], id: id, ...project };
-        // updatedPros.unshift(newProject);
         ...projectData,
         id: projectId,
       };
       return {
         ...prevState,
+        // you want to change the screen and back to <NoProjectSelected/>
         selectedProjectId: undefined,
+        // you want to go to the details of created project
+        // selectedProjectId: projectId,
         projects: [...prevState.projects, newProject],
       };
     });
   }
 
-  // function handleAddTasks(task, projectId) {
-  //   console.log(task);
-  //   setProjects((prevPros) => {
-  //     const updatedPros = prevPros.map((pro) => ({ ...pro }));
-  //     const selectedProject = prevPros.find((item) => item.id == projectId);
-  //     let id =
-  //       selectedProject.tasks.length == 0 ? 0 : selectedProject.tasks[0].id + 1;
-  //     const newTask = { id: id, task: task };
-  //     selectedProject.tasks.unshift(newTask);
-  //     updatedPros.map((pros) => {
-  //       if (pros.id == id) {
-  //         pros.tasks = selectedProject.tasks;
-  //       }
-  //     });
-  //     return updatedPros;
-  //   });
-  // }
-
-  // function handleDeleteTask(taskId, projectId) {
-  //   console.log(
-  //     "Deleting task with ID:",
-  //     taskId,
-  //     "from project with ID:",
-  //     projectId
-  //   );
-  //   setProjects((prevPros) => {
-  //     const updatedPros = prevPros.map((project) => {
-  //       if (project.id === projectId) {
-  //         const updatedTasks = project.tasks.filter(
-  //           (task) => task.id !== taskId
-  //         );
-  //         console.log("Updated tasks for project:", projectId, updatedTasks);
-  //         return { ...project, tasks: updatedTasks };
-  //       }
-  //       return project;
-  //     });
-  //     console.log("Updated projects:", updatedPros);
-  //     return updatedPros;
-  //   });
-  //}
-
-  //   useEffect(() => {
-  //     if (selectedProject) {
-  //       const updatedProject = projects.find(
-  //         (item) => item.id === selectedProject.id
-  //       );
-  //       console.log("Updating selected project:", updatedProject);
-  //       setSelectedProject(updatedProject);
-  //     }
-  //   }, [projects, selectedProject]);
-
   //   function handleDeleteTask(taskId, projectId) {
   //     setProjects((prevPros) => {
-  //       // Create a deep copy of projects array
+  // Create a deep copy of projects array
   //       const updatedPros = prevPros.map((pro) => ({ ...pro }));
 
-  //       // Find the project to update
+  // Find the project to update
   //       const projectToUpdate = updatedPros.find((item) => item.id === projectId);
   //       if (projectToUpdate) {
-  //         // Create a new tasks array excluding the deleted task
+  // Create a new tasks array excluding the deleted task
   //         const updatedTasks = projectToUpdate.tasks.filter(
   //           (task) => task.id !== taskId
   //         );
 
-  //         // Update the project's tasks array immutably
+  // Update the project's tasks array immutably
   //         projectToUpdate.tasks = updatedTasks;
 
-  //         // Return a new array of projects with the updated project
+  // Return a new array of projects with the updated project
   //         return updatedPros;
   //       }
-  //       // Return the original projects if no update was made
+  // Return the original projects if no update was made
   //       return prevPros;
   //     });
   //Conclusion
