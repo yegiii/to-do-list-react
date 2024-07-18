0d5bd5a3e3fb82ec0419de8dbd46876bcdf46f15
Yeganeh <56545979+yegiii@users.noreply.github.com>
2024-07-18 06:29:20 +0330

    Tidy up

--- src/components/ProjectsSidebar.jsx
+++ src/components/ProjectsSidebar.jsx
@@ -12,7 +12,7 @@ export default function ProjectsSidebar({ onStartAddProject, projects }) {
       <ul className="mt-8 ">
         {projects.map((project) => {
           return (
-            <li onClick={() => handleShowProject(project.id)} key={project.id}>
+            <li key={project.id}>
               <button className="w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800">
                 {project.title}
               </button>
